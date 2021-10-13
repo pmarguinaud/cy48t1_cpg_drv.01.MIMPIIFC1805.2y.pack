8b5af5bda6cf1a9020b8b048215e265ae567ad7a
Philippe Marguinaud <philippe.marguinaud@meteo.fr>
2021-10-13 11:17:59 +0000

    Introduce MF_PHYS_TYPE

--- pass.sh
+++ pass.sh
@@ -3,13 +3,9 @@
 for f in \
    arpifs/phys_dmn/mf_phys.F90    \
    arpifs/adiab/cpg.F90           \
-   arpifs/adiab/cpg_gp.F90        \
-   arpifs/adiab/cpg_dyn.F90       \
    arpifs/adiab/cpg_dia.F90       \
-   arpifs/dia/cpdysldia.F90       \
    arpifs/phys_dmn/aplpar.F90     \
-   arpifs/phys_dmn/apl_arome.F90  \
-   arpifs/phys_dmn/mf_phys_prep.F90 
+   arpifs/phys_dmn/apl_arome.F90  
 do
   ./pass.pl src/local/$f
   \mv src/local/$f.new src/local/$f

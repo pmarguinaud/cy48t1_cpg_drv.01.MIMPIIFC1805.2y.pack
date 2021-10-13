eb9434714b947756210035e99591e99dd8ffefee
Philippe Marguinaud <philippe.marguinaud@meteo.fr>
2021-10-13 14:53:12 +0000

    Remove pointers in cpg_gp.F90

---
+++ mf_phys_pp.pl
@@ -0,0 +1,43 @@
+#!/home/gmap/mrpm/marguina/install/perl-5.32.0/bin/perl -w
+#
+use strict;
+use FindBin qw ($Bin);
+use Data::Dumper;
+use FileHandle;
+use File::Basename;
+use lib $Bin;
+use Fxtran;
+
+my $F90 = 'src/local/arpifs/adiab/cpg_gp.F90';
+
+my $d = &Fxtran::fxtran (location => $F90, fopts => [qw (-line-length 300)]);
+
+my @ap = &F ('//pointer-a-stmt', $d);
+
+for my $ap (@ap)
+  {
+    my ($E1) = &F ('./E-1/ANY-E', $ap);
+    my ($E2) = &F ('./E-2/ANY-E', $ap);
+    next if ($E2->textContent =~ m/\(/o);
+
+
+    my ($N1) = &F ('./N', $E1, 1);
+
+    my @expr = &F ('//named-E/N[string(.)="?"]', $N1, $d);
+
+    for my $expr (@expr)
+      {
+        my $stmt = &Fxtran::stmt ($expr);
+        next if ($stmt->isSameNode ($ap));
+        if ($stmt->nodeName eq 'call-stmt')
+          {
+            my ($p) = &F ('./procedure-designator', $stmt, 1);
+#           next if ($p =~ m/^(?:APLPAR|APL_AROME)$/o);
+          }
+        $expr->replaceNode (&t ($E2->textContent));
+      }
+
+  }
+
+'FileHandle'->new (">$F90.new")->print ($d->textContent);
+
---
+++ mf_phys_pp1.pl
@@ -0,0 +1,53 @@
+#!/home/gmap/mrpm/marguina/install/perl-5.32.0/bin/perl -w
+#
+use strict;
+use FindBin qw ($Bin);
+use Data::Dumper;
+use FileHandle;
+use File::Basename;
+use lib $Bin;
+use Fxtran;
+
+my $F90 = 'src/local/arpifs/phys_dmn/mf_phys.F90';
+
+my $d = &Fxtran::fxtran (location => $F90, fopts => [qw (-line-length 300)]);
+
+my @ap = &F ('//pointer-a-stmt', $d);
+
+for my $ap (@ap)
+  {
+    my ($E1) = &F ('./E-1/ANY-E', $ap);
+    my ($E2) = &F ('./E-2/ANY-E', $ap);
+    next if ($E2->textContent =~ m/\(/o);
+
+
+    my ($N1) = &F ('./N', $E1, 1);
+
+    my @expr = &F ('//named-E/N[string(.)="?"]', $N1, $d);
+
+    next unless (scalar (@expr) == 1);
+
+    print $N1, "\n";
+
+    my ($en_decl) = &F ('//EN-decl[string(EN-N)="?"]', $N1, $d);
+
+    if ($en_decl->nextSibling)
+      {
+        $en_decl->nextSibling->unbindNode ();
+        $en_decl->unbindNode ();
+      }
+    elsif ($en_decl->previousSibling)
+      {
+        $en_decl->previousSibling->unbindNode ();
+        $en_decl->unbindNode ();
+      }
+    else
+      {
+        &Fxtran::stmt ($en_decl)->unbindNode ();
+      }
+
+    $ap->unbindNode ();
+  }
+
+'FileHandle'->new (">$F90.new")->print ($d->textContent);
+

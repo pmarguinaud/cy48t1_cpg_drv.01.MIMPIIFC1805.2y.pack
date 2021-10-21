85c53b70c15dd1e51777db2e199efdac7ecda41f
Philippe Marguinaud <philippe.marguinaud@meteo.fr>
2021-10-21 06:29:26 +0000

    Remove local pointers in cpg_gp.F90 (one remaining)

--- check.sh
+++ check.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
+let "i=0"
+
+F=""
 
 for f in $(ls slurm-*.out | sort -r)
 do
-echo $f
-grep NORMDIFF $f | grep -v 0.000000000e+00 | grep -v REF
-#break
+F="$f $F"
+let "i=$i+1"
+if [ $i -eq 2 ]
+then
+  break
+fi
 done
 
+grep NORMDIFF $F
 

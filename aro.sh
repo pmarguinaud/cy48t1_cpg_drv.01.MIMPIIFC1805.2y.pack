0fc220eb2ad30bc9ddea7930e0ebc422c3554f03
Philippe Marguinaud <philippe.marguinaud@meteo.fr>
2021-10-22 14:39:31 +0000

    Add tests for AROME/NH & LTWOTL=F

--- aro.sh
+++ aro.sh
@@ -201,7 +201,7 @@ cat fort.4
 # Run the model; use your mpirun
 
 pack=$PACK
-#ack=/home/mf/dp/marp/gco/packs/cy48t1_main.01.MIMPIIFC1805.2y.pack
+#pack=/home/mf/dp/marp/gco/packs/cy48t1_main.01.MIMPIIFC1805.2y.pack
 
 /opt/softs/mpiauto/mpiauto --verbose --wrap --wrap-stdeo --nouse-slurm-mpi --prefix-mpirun '/usr/bin/time -f "time=%e"' \
     --nnp $NTASK_FC --nn $NNODE_FC --openmp $NOPMP_FC -- $pack/bin/MASTERODB \
@@ -209,7 +209,8 @@ pack=$PACK
 
 ls -lrt
 
-#diffNODE.001_01 NODE.001_01 $PACK/NODE.001_01.$NAM1
+diffNODE.001_01 NODE.001_01 $PACK/NODE.001_01.$NAM1.$NAM2
+#cp NODE.001_01 $PACK/NODE.001_01.$NAM1.$NAM2
 
 cd ..
 

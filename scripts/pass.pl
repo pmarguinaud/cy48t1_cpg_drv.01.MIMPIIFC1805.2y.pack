920668691970f796a8dd3371a6925858500300aa
Philippe Marguinaud <philippe.marguinaud@meteo.fr>
2021-10-25 16:02:16 +0000

    Use structures in lacdyn.F90

--- scripts/pass.pl
+++ scripts/pass.pl
@@ -8,12 +8,12 @@ use File::Basename;
 use lib $Bin;
 use Fxtran;
 
-my ($var, $typ, $mod) = qw (YDVARS FIELD_VARIABLES FIELD_VARIABLES_MOD);
+#my ($var, $typ, $mod) = qw (YDVARS FIELD_VARIABLES FIELD_VARIABLES_MOD);
 #my ($var, $typ, $mod) = qw (YDSURFVARS SURFACE_VARIABLES SURFACE_VARIABLES_MOD);
 #my ($var, $typ, $mod) = qw (YDMF_PHYS_SURF MFPHYS_SURF_TYPE MFPHYS_SURFACE_TYPE_MOD);
 #my ($var, $typ, $mod) = qw (YDCPG_DYN9 CPG_DYN_TYPE CPG_DYN_TYPE_MOD);
 #my ($var, $typ, $mod) = qw (YDMF_PHYS MF_PHYS_TYPE MF_PHYS_TYPE_MOD);
-#my ($var, $typ, $mod) = qw (YDCPG_PHY0 CPG_PHY_TYPE CPG_DYN_TYPE_MOD);
+my ($var, $typ, $mod) = qw (YDCPG_PHY0 CPG_PHY_TYPE CPG_DYN_TYPE_MOD);
 
 
 #my @r = qw (MF_PHYS CPG CPG_GP CPG_DYN CPG_DIA MF_PHYS_PREP CPDYSLDIA);

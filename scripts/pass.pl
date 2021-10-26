a7519dcbfb07b38c19a081d2b31635e7294e89ed
Philippe Marguinaud <philippe.marguinaud@meteo.fr>
2021-10-26 17:03:54 +0000

    Wrap  & PGDEOSI,PGUEOSI,PGMU0,PGMU0_MIN,PGMU0_MAX,&  & PGDEOTI,PGDEOTI2,PGUEOTI,PGUEOTI2,PGEOLT,PGEOXT,&  & PGRPROX,PGMIXP,PGFLUXC,PGRSURF,&

--- scripts/pass.pl
+++ scripts/pass.pl
@@ -14,12 +14,13 @@ use Fxtran;
 #my ($var, $typ, $mod) = qw (YDCPG_DYN9 CPG_DYN_TYPE CPG_DYN_TYPE_MOD);
 #my ($var, $typ, $mod) = qw (YDMF_PHYS MF_PHYS_TYPE MF_PHYS_TYPE_MOD);
 #my ($var, $typ, $mod) = qw (YDCPG_TND CPG_TND_TYPE CPG_DYN_TYPE_MOD);
-my ($var, $typ, $mod) = qw (YDCPG_MISC CPG_MISC_TYPE CPG_DYN_TYPE_MOD);
+#my ($var, $typ, $mod) = qw (YDCPG_MISC CPG_MISC_TYPE CPG_DYN_TYPE_MOD);
 #my ($var, $typ, $mod) = qw (YDCPG_TND CPG_TND_TYPE CPG_DYN_TYPE_MOD);
+my ($var, $typ, $mod) = qw (YDCPG_OPT CPG_OPT_TYPE CPG_DYN_TYPE_MOD);
 
 
 #my @r = qw (MF_PHYS CPG CPG_GP CPG_DYN CPG_DIA MF_PHYS_PREP CPDYSLDIA);
-my @r = qw (CPG_GP CPG_DIA CPG_DYN );
+my @r = qw (MF_PHYS APLPAR APL_AROME);
 
 my $F90 = shift;
 

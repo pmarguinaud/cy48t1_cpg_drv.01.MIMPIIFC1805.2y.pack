5d0a9c5f592f7bcdc198c9652b8f135240d89c36
Philippe Marguinaud <philippe.marguinaud@meteo.fr>
2022-01-28 09:26:03 +0000

    Use YDCST directly in calculations

---
+++ src/local/arpifs/function/fcttrm.func.h
@@ -0,0 +1,19 @@
+#define _RALPD_ RALPD
+#define _RALPS_ RALPS
+#define _RALPW_ RALPW
+#define _RBETD_ RBETD
+#define _RBETS_ RBETS
+#define _RBETW_ RBETW
+#define _RCPV_  RCPV
+#define _RCS_   RCS
+#define _RCW_   RCW
+#define _RETV_  RETV
+#define _RGAMD_ RGAMD
+#define _RGAMS_ RGAMS
+#define _RGAMW_ RGAMW
+#define _RLSTT_ RLSTT
+#define _RLVTT_ RLVTT
+#define _RTT_   RTT
+#define _RV_    RV
+
+#include "fcttrm.base.h"
---
+++ src/local/arpifs/function/fcttrm.ycst.h
@@ -0,0 +1,19 @@
+#define _RALPD_ YDCST%RALPD
+#define _RALPS_ YDCST%RALPS
+#define _RALPW_ YDCST%RALPW
+#define _RBETD_ YDCST%RBETD
+#define _RBETS_ YDCST%RBETS
+#define _RBETW_ YDCST%RBETW
+#define _RCPV_  YDCST%RCPV
+#define _RCS_   YDCST%RCS
+#define _RCW_   YDCST%RCW
+#define _RETV_  YDCST%RETV
+#define _RGAMD_ YDCST%RGAMD
+#define _RGAMS_ YDCST%RGAMS
+#define _RGAMW_ YDCST%RGAMW
+#define _RLSTT_ YDCST%RLSTT
+#define _RLVTT_ YDCST%RLVTT
+#define _RTT_   YDCST%RTT
+#define _RV_    YDCST%RV
+
+#include "fcttrm.base.h"

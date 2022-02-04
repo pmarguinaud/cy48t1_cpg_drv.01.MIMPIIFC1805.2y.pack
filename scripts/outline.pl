76e313097d5bdffe670882d37511048d1f6b4e56
Philippe Marguinaud <philippe.marguinaud@meteo.fr>
2022-02-04 14:06:56 +0000

    +apl_arpege_init.F90

--- scripts/outline.pl
+++ scripts/outline.pl
@@ -94,7 +94,7 @@ sub sortArgs
   return (@d, @l);
 }
 
-my @INTRINSIC = qw (SIGN MAX MIN MOD JPRB REAL);
+my @INTRINSIC = qw (SIGN MAX MIN MOD JPRB REAL EXP ASIN FOLH SQRT);
 my %INTRINSIC = map { ($_, 1) } @INTRINSIC;
 
 

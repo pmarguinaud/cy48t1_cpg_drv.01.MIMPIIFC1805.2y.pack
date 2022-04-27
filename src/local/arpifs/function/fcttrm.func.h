95b9754cd2f391ef32cfd8c9fc24b07f3c998e78
Philippe Marguinaud <philippe.marguinaud@meteo.fr>
2022-04-27 11:08:17 +0000

    Use PREFIX macro in fcttrm.*.h

--- src/local/arpifs/function/fcttrm.func.h
+++ src/local/arpifs/function/fcttrm.func.h
@@ -1,38 +1,5 @@
-#define _RALPD_ RALPD
-#define _RALPS_ RALPS
-#define _RALPW_ RALPW
-#define _RBETD_ RBETD
-#define _RBETS_ RBETS
-#define _RBETW_ RBETW
-#define _RCPV_  RCPV
-#define _RCS_   RCS
-#define _RCW_   RCW
-#define _RETV_  RETV
-#define _RGAMD_ RGAMD
-#define _RGAMS_ RGAMS
-#define _RGAMW_ RGAMW
-#define _RLSTT_ RLSTT
-#define _RLVTT_ RLVTT
-#define _RTT_   RTT
-#define _RV_    RV
+#define _PREFIX_
 
 #include "fcttrm.base.h"
 
-#undef _RALPD_ 
-#undef _RALPS_ 
-#undef _RALPW_ 
-#undef _RBETD_ 
-#undef _RBETS_ 
-#undef _RBETW_ 
-#undef _RCPV_  
-#undef _RCS_   
-#undef _RCW_   
-#undef _RETV_  
-#undef _RGAMD_ 
-#undef _RGAMS_ 
-#undef _RGAMW_ 
-#undef _RLSTT_ 
-#undef _RLVTT_ 
-#undef _RTT_   
-#undef _RV_    
-
+#undef _PREFIX_
--- src/local/arpifs/function/fcttrm.ycst.h
+++ src/local/arpifs/function/fcttrm.ycst.h
@@ -1,38 +1,5 @@
-#define _RALPD_ YDCST%RALPD
-#define _RALPS_ YDCST%RALPS
-#define _RALPW_ YDCST%RALPW
-#define _RBETD_ YDCST%RBETD
-#define _RBETS_ YDCST%RBETS
-#define _RBETW_ YDCST%RBETW
-#define _RCPV_  YDCST%RCPV
-#define _RCS_   YDCST%RCS
-#define _RCW_   YDCST%RCW
-#define _RETV_  YDCST%RETV
-#define _RGAMD_ YDCST%RGAMD
-#define _RGAMS_ YDCST%RGAMS
-#define _RGAMW_ YDCST%RGAMW
-#define _RLSTT_ YDCST%RLSTT
-#define _RLVTT_ YDCST%RLVTT
-#define _RTT_   YDCST%RTT
-#define _RV_    YDCST%RV
+#define _PREFIX_ YDCST%
 
 #include "fcttrm.base.h"
 
-#undef _RALPD_ 
-#undef _RALPS_ 
-#undef _RALPW_ 
-#undef _RBETD_ 
-#undef _RBETS_ 
-#undef _RBETW_ 
-#undef _RCPV_  
-#undef _RCS_   
-#undef _RCW_   
-#undef _RETV_  
-#undef _RGAMD_ 
-#undef _RGAMS_ 
-#undef _RGAMW_ 
-#undef _RLSTT_ 
-#undef _RLVTT_ 
-#undef _RTT_   
-#undef _RV_    
-
+#undef _PREFIX_

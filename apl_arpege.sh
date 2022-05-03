1d78ec94ffb3f85861bf9bdc803eb197f2becd69
Philippe Marguinaud <philippe.marguinaud@meteo.fr>
2022-05-03 15:03:36 +0000

    More parallel routines

--- apl_arpege.sh
+++ apl_arpege.sh
@@ -13,7 +13,11 @@ for f in \
   src/local/arpifs/phys_dmn/apl_arpege_aerosols_for_radiation.F90 \
   src/local/arpifs/phys_dmn/apl_arpege_cloudiness.F90 \
   src/local/arpifs/phys_dmn/apl_arpege_radiation.F90 \
-  src/local/arpifs/phys_dmn/apl_arpege_soil_hydro.F90
+  src/local/arpifs/phys_dmn/apl_arpege_soil_hydro.F90 \
+  src/local/arpifs/phys_dmn/apl_arpege_deep_convection.F90 \
+  src/local/arpifs/phys_dmn/apl_arpege_surface.F90 \
+  src/local/arpifs/phys_dmn/apl_arpege_precipitation.F90 \
+  src/local/arpifs/phys_dmn/apl_arpege_hydro_budget.F90 
 do
 ./scripts/apl_arpege.pl $f
 done
--- scripts/apl_arpege.pl
+++ scripts/apl_arpege.pl
@@ -502,7 +502,7 @@ for my $n (sort keys (%$t))
 
 &setupLocalFields ($doc, $t);
 
-shift (@call) for (1 .. 12);
+shift (@call) for (1 .. 14);
 
 for my $call (@call)
   {

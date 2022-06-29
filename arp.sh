1e53e8959b85dd7854b2682a998d744482e4b13d
Philippe Marguinaud <philippe.marguinaud@meteo.fr>
2022-06-29 13:09:18 +0000

    arp.sh + aro.sh use ref.pgi

--- arp.sh
+++ arp.sh
@@ -197,7 +197,8 @@ pack=/home/gmap/mrpm/marguina/pack/48t1_cpg_drv.01.PGI217.cpu0
     --nnp $NTASK_FC --nn $NNODE_FC --openmp $NOPMP_FC -- $pack/bin/MASTERODB \
  -- --nnp $NTASK_IO --nn $NNODE_IO --openmp $NOPMP_IO -- $pack/bin/MASTERODB 
 
-diffNODE.001_01 NODE.001_01 $PACK/ref/NODE.001_01.$NAM
+diffNODE.001_01 NODE.001_01 $pack/ref.pgi/NODE.001_01.$NAM
+#cp NODE.001_01 $pack/ref.pgi/NODE.001_01.$NAM
 
 ls -lrt
 

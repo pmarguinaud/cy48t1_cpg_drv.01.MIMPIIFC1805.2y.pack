73cbbf31a6a9e579d7dd7072f51ec548d3595963
Philippe Marguinaud <philippe.marguinaud@meteo.fr>
2022-07-06 15:33:36 +0000

    Add LCDERS to variables

--- fypp.sh
+++ fypp.sh
@@ -27,6 +27,11 @@ do
     d=arpifs/adiab
   fi
 
+  if [ "$b" = "test_ydvars" ]
+  then
+    d=arpifs/adiab
+  fi
+
 
   /opt/softs/anaconda3/bin/fypp -m os -M . -m yaml -m field_config ./$b.fypp ./$b.F90
 

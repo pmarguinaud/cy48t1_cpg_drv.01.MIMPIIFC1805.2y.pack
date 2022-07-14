207df848b42f3ed6cfdea8019bf6eb29cd10c3d7
Philippe Marguinaud <philippe.marguinaud@meteo.fr>
2022-07-14 16:03:09 +0000

    More arguments to lanhsib.F90

--- scripts/cpg_gp1.pl
+++ scripts/cpg_gp1.pl
@@ -22,7 +22,7 @@ for my $F90 (@ARGV)
 
     for my $expr (@expr)
       {
-        print $expr->textContent, "\n";
+#       print $expr->textContent, "\n";
         my ($r) = &F ('./R-LT/ANY-R', $expr);
         goto NEXT unless ($r);
 
@@ -33,14 +33,14 @@ for my $F90 (@ARGV)
             ($ind) = &F ('./section-subscript-LT/section-subscript[last()]/lower-bound/ANY-E', $r);
             $indp = $ind->parentNode->parentNode;
 
-            print $ind->textContent, "\n";
+#           print $ind->textContent, "\n";
           }
         elsif ($r->nodeName eq 'parens-R')
           {
             ($ind) = &F ('./element-LT/element[last()]/ANY-E', $r);
             $indp = $ind->parentNode;
 
-            print $ind->textContent, "\n";
+#           print $ind->textContent, "\n";
           }
 
         my ($N) = &F ('./N', $expr);
@@ -110,124 +110,80 @@ for my $F90 (@ARGV)
         $r->unbindNode if ($r->textContent eq '(:)');
 
 NEXT:
-        print "\n";
+#       print "\n";
       }
  
 
-=pod
+    @expr = &F ('.//named-E[string(N)="PB1"][R-LT]', $d);
 
-    for my $call (@call)
+    for my $expr (@expr) 
       {
-        my ($argspec) = &F ('./arg-spec', $call);
-        my @arg = &F ('./arg/ANY-E', $argspec);
-        next unless (scalar (@arg) == 3);
-    
-        my $stmt;
-    
-        if ($arg[1]->textContent =~ m/^(?:PSP_(?:RR|SB|SG)|PSD_(?:VF|VA|VD|VH|VK|VP|VV))$/o)
-          {
-            my ($p, $x, $v) = map ({ $_->textContent } @arg);
-            $x =~ s/^PS(P|D)_//o; my $z = $1;
-            $p =~ s/^.*%Y//o;
-    
-            my ($f, $t) = split (m/%/o, $p);
-            $t =~ s/^MP/T/o;
-            $t = "T0" if ($t eq 'T');
+        my ($r) = &F ('./R-LT/parens-R', $expr);
+        my ($rlt) = $r->parentNode;
 
-            $t = '' if ($z eq 'D');
-    
-            $t = $t ? "_$t" : "";
-            
-            my @ss = &F ('//EN-decl[string (EN-N)="?"]//shape-spec', $v, $d);
-#           print &Dumper ([$f, $t, $x, $v, scalar (@ss)]);
-    
-    
-            $stmt = &n ("<pointer-a-stmt><E-1><named-E><N><n>$v</n></N></named-E></E-1> "
-                      . "<a>=&gt;</a> <E-2>"
-                      . "<named-E><N><n>YDMF_PHYS_SURF</n></N><R-LT><component-R>%<ct>GS${z}_$x</ct></component-R>"
-                      . "<component-R>%<ct>P${f}$t</ct></component-R></R-LT></named-E>"
-                      . "</E-2></pointer-a-stmt>");
-    
-    
-          }
-        elsif ($arg[1]->textContent =~ m/^PGMV/o)
-          {
-            (my $p, undef, my $v) = map ({ $_->textContent } @arg);
+        die unless ($r);
+        my @ind = &F ('./element-LT/element/ANY-E', $r);
+        die $r unless (scalar (@ind) == 2);
+        die $expr unless (($ind[0]->textContent eq 'JROF') && ($ind[1]->nodeName eq 'op-E'));
+        my @e = &F ('.//named-E', $ind[1]);
+        die $ind[1] unless (scalar (@e) == 3);
+        die unless (($e[1]->textContent eq 'JLEV') && ($e[2]->textContent eq 'NFLSA'));
+        my $M = $e[0]->textContent;
+        die unless ($M =~ s/^MSLB1//o);
 
+        my ($N) = &F ('./N/n/text()', $expr);
 
-            my ($t, $y) = split (m/%/o, $p);
-            $t =~ s/^Y//o;
-            $y =~ s/^M//o; 
-            if ($y =~ s/(L|M)$//o)
-              {
-                my $d = $1;
-                next unless (($t eq 'T0') || ($t eq 'T9'));
-                if ($t eq 'T0')
-                  {
-                    $t = "D$d";
-                  }
-                else
-                  {
-                    $t = "D${d}9";
-                  }
-              }
-    
-
-            if (($y eq 'SGRT') && ($t =~ m/^(?:DL|DM)9?$/o))
-              {
-                my ($d, $T) = ($t =~ m/^D(M|L)(9)?$/o);
-                $T ||= '0';
-                $y = "$y$d";
-                $t = "T$T";
-              }
-
-            $stmt = &n ("<pointer-a-stmt><E-1><named-E><N><n>$v</n></N></named-E></E-1> <a>=&gt;</a> "
-                      . "<E-2><named-E><N><n>YDVARS</n></N><R-LT><component-R>%<ct>$y</ct></component-R>"
-                      . "<component-R>%<ct>$t</ct></component-R></R-LT></named-E></E-2></pointer-a-stmt>");
-    
-          }
-        elsif ($arg[1]->textContent =~ m/^PGFL/o)
-          {
-            (my $p, undef, my $v) = map ({ $_->textContent } @arg);
-            my ($y, $t) = split (m/%/o, $p);
-            $t =~ s/^MP//o or die "t=$t";
-            $t ||= '0';
-            $y =~ s/^Y//o;
-    
-    
-            die unless (grep { $_ eq $y } @fn);
-    
-            if ($t =~ m/^\d$/o)
-              {
-                $t = "T$t";
-              }
-            elsif (($t eq 'L') or ($t eq 'M'))
-              {
-                $t = "D$t";
-              }
-            else
-              {
-                die;
-              }
-    
-            $stmt = &n ("<pointer-a-stmt><E-1><named-E><N><n>$v</n></N></named-E></E-1> <a>=&gt;</a> "
-                      . "<E-2><named-E><N><n>YDVARS</n></N><R-LT><component-R>%<ct>$y</ct></component-R>"
-                      . "<component-R>%<ct>$t</ct></component-R></R-LT></named-E></E-2></pointer-a-stmt>");
-    
-          }
-    
-        if ($stmt)
-          {
-            my ($E1) = &F ('./E-1', $stmt);
-            my $sp = $E1->nextSibling;
-            $sp->setData (' ' x (12 - length ($E1->textContent)));
-            $call->replaceNode ($stmt);
-          }
-
-
+        $N->setData ('YDCPG_SL1');
+        $rlt->insertBefore (&n ("<component-R>%<ct>$M</ct></component-R>"), $rlt->firstChild);
+        $ind[1]->replaceNode (&n ('<named-E><N><n>JLEV</n></N></named-E>'));
       }
-        
-=cut
+
+    
+    @expr = &F ('.//named-E[string(N)="PB2"][R-LT]', $d);
+
+    for my $expr (@expr) 
+      {
+        my ($r) = &F ('./R-LT/parens-R', $expr);
+        my ($rlt) = $r->parentNode;
+
+        die unless ($r);
+        my @ind = &F ('./element-LT/element/ANY-E', $r);
+        die $r unless (scalar (@ind) == 2);
+        if ($ind[1]->nodeName eq 'named-E')
+          {
+            die &Dumper ([$expr->textContent, [map { $_->textContent } @ind]]) 
+              unless (($ind[0]->textContent eq 'JROF') && ($ind[1]->nodeName eq 'named-E'));
+
+            my $M = $ind[1]->textContent;
+            die unless ($M =~ s/^MSLB2//o);
+
+            my ($N) = &F ('./N/n/text()', $expr);
+
+            $N->setData ('YDCPG_SL2');
+            $rlt->insertBefore (&n ("<component-R>%<ct>$M</ct></component-R>"), $rlt->firstChild);
+            $ind[1]->parentNode->previousSibling->unbindNode;
+            $ind[1]->parentNode->unbindNode;
+          }
+        else
+          {
+            die &Dumper ([$expr->textContent, [map { $_->textContent } @ind]]) 
+              unless (($ind[0]->textContent eq 'JROF') && ($ind[1]->nodeName eq 'op-E'));
+            my @e = &F ('.//named-E', $ind[1]);
+            die $ind[1] unless (scalar (@e) == 2);
+            die unless ($e[1]->textContent eq 'JLEV');
+            my $M = $e[0]->textContent;
+            die unless ($M =~ s/^MSLB2//o);
+
+            my ($N) = &F ('./N/n/text()', $expr);
+
+            $N->setData ('YDCPG_SL2');
+            $rlt->insertBefore (&n ("<component-R>%<ct>$M</ct></component-R>"), $rlt->firstChild);
+            $ind[1]->replaceNode (&n ('<named-E><N><n>JLEV</n></N></named-E>'));
+          }
+      }
+
+    
+#   print $d->textContent;
     
     'FileHandle'->new (">$F90.new")->print ($d->textContent);
     

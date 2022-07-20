91a3a49e0953cc46f4aee359872ee8ca024e2b64
Philippe Marguinaud <philippe.marguinaud@meteo.fr>
2022-07-20 14:49:15 +0000

    Add fields for spline interpolation in CPG_SL1_TYPE

--- fypp.sh
+++ fypp.sh
@@ -62,6 +62,11 @@ do
     d=arpifs/adiab
   fi
 
+  if [ "$b" = "lattex_expl_vspltrans" ]
+  then
+    d=arpifs/adiab
+  fi
+
 
   /opt/softs/anaconda3/bin/fypp -m os -M . -m yaml -m field_config ./$b.fypp ./$b.F90
 
